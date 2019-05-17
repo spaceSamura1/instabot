75b9078c1640b8571c0f92cebac0d165268d94e0
T-Streets <tyler@featherandash.io>
2019-05-16 23:59:40 -0700

    refactors for error handling

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ class Bot:
         likes = self.likes
         comments = self.comments
         tag = 0
-        hashtag_list = ['digitalmarketing', 'contentmarketing', 'experiential', 'influencer', 'marketingdigital']
+        hashtag_list = ['influencer', 'marketingdigital']
         text = ['This is great, nice work!', 'This looks so cool! Really well done and very artistic', 'Nice! This is epic.', 
                 'Well this definitely made me stopped scrolling! haha', 'This is art!', 'Really cool, I love this idea' ]
                         
@@ -59,7 +59,7 @@ class Bot:
             sleep(4)
             
             for x in range(1, num_posts):
-                sleep(5) 
+                sleep(randint(6,10)) 
                 #sleep necessary b/c it is searching for username before the page is rendered
 
                 username = driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > header > div.o-MQd > div.PQo_0 > div.e1e1d > h2 > a').text
